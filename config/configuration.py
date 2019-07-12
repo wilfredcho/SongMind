c907cc97d105684d6d76e37c09116c19b902be59
Wilfred Cho <wilfredcho@gmail.com>
2019-07-11 22:05:19 -0700

    update utils

--- config/configuration.py
+++ config/configuration.py
@@ -5,7 +5,7 @@ class Configuration(object):
 
     def __init__(self):
         with open("./config/config.yml", 'r') as ymlfile:
-            self._cfg = yaml.load(ymlfile)
+            self._cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
 
     @property
     def cfg(self):
